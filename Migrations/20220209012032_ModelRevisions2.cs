bef6edb4b8982d50221ef486c219ecde3850675b
MikeMAMR <60702547+MikeMAMR@users.noreply.github.com>
2022-02-11 19:30:34 -0600

    first commit

---
+++ Migrations/20220209012032_ModelRevisions2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    public partial class ModelRevisions2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isVegan",
+                table: "Sauces",
+                newName: "IsVegan");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsVegan",
+                table: "Sauces",
+                newName: "isVegan");
+        }
+    }
+}
